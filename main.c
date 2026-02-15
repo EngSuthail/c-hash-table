6b567c78e4bbda6a5c51a88f60eea1d229a334cc
EngSuthail <e21395@eng.pdn.ac.lk>
2026-02-15 19:50:50 +0530

    Stop tracking .vscode folder and update .gitignore

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hash_table.h"
+#include <stdio.h>
 
 int main(){
     ht_hash_table* ht = ht_new();
     ht_del_hash_table(ht);
+    printf("success!!");
 }
