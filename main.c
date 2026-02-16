f3917e6ba817d83926dc6ed97ec0c8124d6a4144
EngSuthail <e21395@eng.pdn.ac.lk>
2026-02-17 00:26:40 +0530

    test: add insertion verification with bucket traversal

--- main.c
+++ main.c
@@ -3,7 +3,27 @@
 #include "hash_table.h"
 #include <stdio.h>
 
-int main(){
-    printf("The hash value for 'cat' is %d\n", ht_hash("cat", 151, 53));
-    printf("The hash value for 'dog' is %d", ht_hash("dog", 151, 53));
+int main(void) {
+    // Test hash table creation
+    ht_hash_table* ht = ht_new();
+    
+    printf("Hash table created:\n");
+    printf("  Size: %d\n", ht->size);
+    printf("  Count: %d\n", ht->count);
+    
+    // TODO: Test insert/search when implement them
+    ht_insert(ht, "cat", "meow");
+    ht_insert(ht, "dog", "woof");
+    for(int i = 0; i< ht->size; i++){
+        if(ht->items[i] != NULL){
+            ht_item* item = ht->items[i];
+            printf("bucket[%d] | key: %s | value: %s\n", i, item->key, item->value);
+        }
+    }
+    
+    // Cleanup
+    ht_del_hash_table(ht);
+    printf("Hash table deleted successfully\n");
+    
+    return 0;
 }
